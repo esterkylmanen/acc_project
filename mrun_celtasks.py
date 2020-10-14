440447fdf1b3f4616003736408d95c6638ab8896
FwiffoForce <daniel.jedi@yahoo.se>
2020-10-14 11:13:29 +0200

    Support for master node creation

--- mrun_celtasks.py
+++ mrun_celtasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('taskrunner',backend='rpc://',broker='TODO')#'pyamqp://daniel:rabbitpassword@192.168.2.116:5672/lab3_2_vhost')
+app = Celery('taskrunner',backend='rpc://',broker='pyamqp://group8_user:notthatsecretpassword@192.168.29.166:5672/g8vhost')
 
 @app.task
 def placeholder():
