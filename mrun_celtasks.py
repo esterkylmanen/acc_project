40b9f7585f4756cdf3005b82ea48747369b2cc28
FwiffoForce <31653437+FwiffoForce@users.noreply.github.com>
2020-10-14 12:19:32 +0200

    Mini-fix

--- mrun_celtasks.py
+++ mrun_celtasks.py
@@ -1,5 +1,5 @@
 from celery import Celery
-import pyoctave_connector
+import pyoctave_connector as poc
 
 app = Celery('taskrunner',backend='rpc://',broker='pyamqp://group8_user:notthatsecretpassword@192.168.29.153:5672/g8vhost')
 
