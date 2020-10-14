06d948ce10f5bbadd02aeced9400d53eefbc4727
FwiffoForce <31653437+FwiffoForce@users.noreply.github.com>
2020-10-14 12:15:08 +0200

    Indentation
    Indentation was messed up

--- pyoctave_connector.py
+++ pyoctave_connector.py
@@ -2,11 +2,11 @@ import os, sys
 
 def call_octave(funcname,args):
     command = funcname+"(\""+"\",\"".join(args)+"\")"
-	result = os.popen("octave -q --eval \'"+command+"\'").read()
-	results = result.split('result =\n\n',1)[1]
-	time_result = results.strip().split('\n')[0]
-	error_result = results.strip().split('\n')[1].strip() 
-	print('Computation time is {}, Error is {}'.format(time_result, error_result))
+    result = os.popen("octave -q --eval \'"+command+"\'").read()
+    results = result.split('result =\n\n',1)[1]
+    time_result = results.strip().split('\n')[0]
+    error_result = results.strip().split('\n')[1].strip() 
+    return [time_result, error_result]
 
 
 if(__name__ == '__main__'):
