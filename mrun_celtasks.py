d35d7cdcbb96b3b1a759c072341fb21cd0d7d753
FwiffoForce <31653437+FwiffoForce@users.noreply.github.com>
2020-10-14 11:24:58 +0200

    Update mrun_celtasks.py

--- mrun_celtasks.py
+++ mrun_celtasks.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('taskrunner',backend='rpc://',broker='pyamqp://group8_user:notthatsecretpassword@192.168.29.166:5672/g8vhost')
+app = Celery('taskrunner',backend='rpc://',broker='pyamqp://group8_user:notthatsecretpassword@192.168.29.153:5672/g8vhost')
 
 @app.task
 def placeholder():
