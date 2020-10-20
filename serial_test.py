d846f9d44ff99cc592d7601c2f66efcae4f14f61
GirishShanmugam <s.girishshanmugam@gmail.com>
2020-10-20 11:28:36 +0200

    code for serial testing

---
+++ serial_test.py
@@ -0,0 +1,12 @@
+import os, sys, datetime
+
+def call_octave(funcname):
+    start_time = datetime.datetime.now()
+    command = funcname+"()"
+    result = os.popen("octave -q --eval \'"+command+"\'").read()
+    end_time = datetime.datetime.now()
+    print("Time taken {}".format(end_time - start_time))
+
+
+if(__name__ == '__main__'):
+    print(call_octave("Table_test"))
